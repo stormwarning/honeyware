69513f43bee1fb82f7c2f7b67be587c8b43849ba
Jeff <rustyangel@gmail.com>
2019-04-03 09:53:54 -0600

    🎉 Generate project

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        '@zazen/eslint-config',
+        '@zazen/eslint-config/vue',
+        '@nuxtjs',
+        'plugin:nuxt/recommended',
+    ],
+    rules: {},
+}
---
+++ test/specs/Logo.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import test from 'ava'
+
+import Logo from '../../components/Logo.vue'
+
+test('is a Vue instance', (t) => {
+    const wrapper = mount(Logo)
+    t.is(wrapper.isVueInstance(), true)
+})
