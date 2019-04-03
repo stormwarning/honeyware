204964265567072cc5a808368a24df65d1f5c116
Jeff <rustyangel@gmail.com>
2019-04-03 16:24:59 -0600

    Update home screen layout/content

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,86 @@
 <template>
-    <section class="container">
-        <div>
-            <logo />
-            <h1 class="title">
-                honeyware
-            </h1>
-            <h2 class="subtitle">
-                My fabulous Nuxt.js project
-            </h2>
-            <div class="links">
-                <a
-                    href="https://nuxtjs.org/"
-                    target="_blank"
-                    class="button--green"
-                >
-                    Documentation
-                </a>
-                <a
-                    href="https://github.com/nuxt/nuxt.js"
-                    target="_blank"
-                    class="button--grey"
-                >
-                    GitHub
-                </a>
+    <section class="cf pa3 pt4 mw8 center">
+        <header class="ph4-m ph5-l">
+            <h1 class="lh-title f2 fw8 mt0 ttu tracked-mega">Honeyware</h1>
+        </header>
+        <main class="w-100 pa4-m pa5-l mb3 mb5-l bg-white">
+            <div class="measure">
+                <p class="ttu fw8 tracked">
+                    It’s Honeycon 2019.
+                </p>
+                <p class="mt2">
+                    You are going to undertake the greatest heist the world has
+                    ever seen. Just two things:
+                </p>
+                <ol class="ma0 mt3">
+                    <li>
+                        You have a complex plan that requires precise timing
+                    </li>
+                    <li>
+                        You are
+                        <strong>a goddamn bear</strong>
+                    </li>
+                </ol>
             </div>
-        </div>
+
+            <div>
+                <div
+                    v-for="character in characters"
+                    :key="character"
+                    class="fl w-100 w-50-m w-25-l pa3-m pa4-l"
+                >
+                    <article class="bg-white center ba b--black-10">
+                        <div class="pv2 ph3">
+                            <h1 class="f6 ttu tracked">
+                                <nuxt-link :to="/bear/ + character.handle">
+                                    {{ character.handle }}
+                                </nuxt-link>
+                            </h1>
+                        </div>
+                        <div class="pa3">
+                            <button @click="confirmDeleteCharacter(character)">
+                                Delete
+                            </button>
+                        </div>
+                    </article>
+                </div>
+
+                <nuxt-link class="dib mt4 black-60 dim" to="/bear">
+                    Add a Bear
+                </nuxt-link>
+            </div>
+
+            <nuxt-link class="dib mt4 black-60 dim" to="/rules">
+                Read the rules
+            </nuxt-link>
+        </main>
     </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import { mapActions, mapState } from 'vuex'
 
 export default {
-    components: {
-        Logo,
+    computed: {
+        ...mapState({
+            characters: (state) => state.characters,
+        }),
+    },
+
+    mounted() {
+        this.loadCharacters()
+    },
+
+    methods: {
+        ...mapActions(['deleteCharacter', 'loadCharacters']),
+
+        confirmDeleteCharacter(character) {
+            if (
+                confirm(`Are you sure you want to delete ${character.handle}?`)
+            ) {
+                this.deleteCharacter(character)
+            }
+        },
     },
 }
 </script>
-
-<style>
-.container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    margin: 0 auto;
-    text-align: center;
-}
-
-.title {
-    display: block;
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
-        BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
-        sans-serif;
-    font-size: 100px;
-    font-weight: 300;
-    color: #35495e;
-    letter-spacing: 1px;
-}
-
-.subtitle {
-    padding-bottom: 15px;
-    font-size: 42px;
-    font-weight: 300;
-    color: #526488;
-    word-spacing: 5px;
-}
-
-.links {
-    padding-top: 15px;
-}
-</style>
