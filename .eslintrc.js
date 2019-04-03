25930f9ee35456bddbe91eafecbf4f5dceb1b844
Jeff <rustyangel@gmail.com>
2019-04-03 12:31:31 -0600

    Remove Nuxt's ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
     extends: [
         '@zazen/eslint-config',
         '@zazen/eslint-config/vue',
-        '@nuxtjs',
         'plugin:nuxt/recommended',
     ],
     rules: {},
