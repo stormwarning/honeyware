9aa9f9557ce5aa531a4f4680f2e3dc7526744489
Jeff <rustyangel@gmail.com>
2019-04-04 13:26:30 -0600

    🎁 Add Rules page content

---
+++ pages/rules.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="cf pa3 pt4 mw8 center">
+        <header class="ph4-m ph5-l">
+            <h1 class="lh-title f2 fw8 mt0 ttu tracked-mega">
+                Honey Heist rules
+            </h1>
+        </header>
+        <main class="w-100 pa4-m pa5-l mb3 mb5-l bg-white">
+            <div class="measure">
+                <h2 class="f4 ma0 ttu fw8 tracked">Stats</h2>
+                <p>
+                    You have two stats.
+                    <strong>Each starts with 3 points.</strong>
+                </p>
+                <dl>
+                    <dt class="ttu f6 fw8 tracked">Bear</dt>
+                    <dd class="ml0">
+                        Use to maul stuff, run &amp; climb, shrug off damage,
+                        scare people, and generally do bear stuff.
+                    </dd>
+
+                    <dt class="mt2 ttu f6 fw8 tracked">Criminal</dt>
+                    <dd class="ml0">
+                        Use to do anything not directly related to being a bear.
+                    </dd>
+                </dl>
+
+                <h2 class="f4 ma0 mt5 ttu fw8 tracked">Actions</h2>
+                <p>
+                    When you act, and the outcome is in doubt, roll a
+                    <var>d6</var>
+                    . If it’s equal to or under the relevant stat, you succeed.
+                    If it’s over the stat, you fail. If you’re using your bear
+                    special-skill or doing something related to your role, roll
+                    <var>2d6</var>
+                    and pick the lowest.
+                </p>
+
+                <h2 class="f4 ma0 mt5 ttu fw8 tracked">Changing states</h2>
+                <dl>
+                    <dt class="ttu f6 fw8 tracked">Frustration</dt>
+                    <dd class="ml0">
+                        When the plan fails and you run into difficulty, move
+                        one point from
+                        <strong>Criminal</strong>
+                        into
+                        <strong>Bear</strong>
+                        .
+                    </dd>
+                    <dt class="mt2 ttu f6 fw8 tracked">Greed</dt>
+                    <dd class="ml0">
+                        When the plan goes off without a hitch, move one point
+                        from
+                        <strong>Bear</strong>
+                        into
+                        <strong>Criminal</strong>
+                        .
+                    </dd>
+                </dl>
+                <p>
+                    You can voluntarily move one point from
+                    <strong>Bear</strong>
+                    to
+                    <strong>Criminal</strong>
+                    by doing a flashback scene in which you and the other bears
+                    plan out the heist over coffee and cigarrettes in the back
+                    room of a seedy bar.
+                </p>
+                <p>
+                    You can voluntarily move one point from
+                    <strong>Criminal</strong>
+                    to
+                    <strong>Bear</strong>
+                    by eating a load of honey.
+                </p>
+
+                <h2 class="f4 ma0 mt5 ttu fw8 tracked">The end</h2>
+                <p>
+                    If your
+                    <strong>Criminal</strong>
+                    stat ever reaches 6, you are lured into a life of crime and
+                    betray the party. If your
+                    <strong>Bear</strong>
+                    stat ever reaches 6, you flip out bear-style and lose it.
+                    Presumably to be picked up by animal control in half an hour
+                    or so.
+                </p>
+            </div>
+        </main>
+    </section>
+</template>
+
+<script>
+export default {}
+</script>
