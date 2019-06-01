236b6c84df3d67d23dccbc8e2508db5220e1e003
Jeff <rustyangel@gmail.com>
2019-06-01 13:04:22 -0600

    Add SVG gradient for icons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
     <article class="min-vh-100 lh-copy">
+        <svg
+            style="position: absolute; width: 0; height: 0;"
+            aria-hidden="true"
+            focusable="false"
+        >
+            <linearGradient id="honeyGradient" x2="1" y2="1">
+                <stop offset="0%" stop-color="#e67700" />
+                <stop offset="16%" stop-color="#f08400" />
+                <stop offset="33%" stop-color="#fa9100" />
+                <stop offset="50%" stop-color="#ffa007" />
+                <stop offset="66%" stop-color="#ffb318" />
+                <stop offset="83%" stop-color="#ffc328" />
+                <stop offset="100%" stop-color="#ffd43b" />
+            </linearGradient>
+        </svg>
         <nuxt />
     </article>
 </template>
