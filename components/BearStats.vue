e633cdaf7db9531af24d4698a338cabcb39600f0
Jeff <rustyangel@gmail.com>
2019-06-01 13:05:47 -0600

    Add stats and spinner components

---
+++ components/BearStats.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex">
+        <stat-spinner :value="bearStat" />
+
+        <div></div>
+
+        <stat-spinner :value="criminalStat" />
+    </div>
+</template>
+
+<script>
+import StatSpinner from '~/components/StatSpinner'
+
+export default {
+    components: {
+        StatSpinner,
+    },
+
+    props: {
+        stats: {
+            type: Object,
+            default: () => ({ bear: 3, criminal: 3 }),
+        },
+    },
+
+    computed: {
+        bearStat() {
+            return this.stats.bear
+        },
+        criminalStat() {
+            return this.stats.criminal
+        },
+    },
+}
+</script>
---
+++ components/StatSpinner.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="h4 overflow-hidden">
+            <div class="flex flex-column">
+                <span
+                    v-for="n in 6"
+                    :key="n"
+                    :class="[n === value ? 'o-100' : 'o-20']"
+                    :aria-hidden="n === value ? null : true"
+                    class="flex items-center justify-center w4 mv2 f2 lh-solid"
+                    >{{ n }}</span
+                >
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Number,
+            default: 3,
+        },
+    },
+
+    data: () => {
+        return {
+            newValue: null,
+        }
+    },
+
+    methods: {
+        updateValue() {
+            this.$emit('newValue')
+        },
+    },
+}
+</script>
