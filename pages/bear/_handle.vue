df06df78ea86e38b8930f45135e8bf9f6ba829bc
Jeff <rustyangel@gmail.com>
2019-06-01 13:07:34 -0600

    Update character page layout

--- pages/bear/_handle.vue
+++ pages/bear/_handle.vue
@@ -2,18 +2,36 @@
     <section class="mw8 center">
         <page-header :title="selectedCharacter.handle" />
         <main class="pa3 pa4-ns">
-            {{ selectedCharacter.handle }}
-            <br />
-            {{ selectedCharacter.typeString }}
-            <br />
-            {{ selectedCharacter.skillString }}
-            <br />
-            {{ selectedCharacter.descriptorString }}
-            <br />
-            {{ selectedCharacter.roleString }}
-            <br />
-            {{ selectedCharacter.hatString }}
-            <br />
+            <p class="measure f5 f4-ns lh-copy">
+                A
+                <strong>{{ selectedCharacter.typeString }}</strong>
+                who can
+                <strong>{{ selectedCharacter.skillString }}</strong
+                >. The
+                <strong>
+                    {{ selectedCharacter.descriptorString }}
+                    {{ selectedCharacter.roleString }}
+                </strong>
+                of the crew.
+                <span v-if="selectedCharacter.hatString">
+                    Wears a
+                    <strong>{{ selectedCharacter.hatString }}</strong
+                    >.
+                </span>
+            </p>
+            <div class="flex flex-wrap items-start pv4 nr4 nl4">
+                <section
+                    class="flex-grow-1 flex-shrink-1 w-40 pa4 ma1 bg-grey-800"
+                >
+                    <h2 class="ma0 f4 fw5">Stats</h2>
+                    <bear-stats :stats="selectedCharacter.stats" />
+                </section>
+                <section
+                    class="flex-grow-1 flex-shrink-1 w-40 pa4 ma1 bg-grey-800"
+                >
+                    <h2 class="ma0 f4 fw5">Reminders</h2>
+                </section>
+            </div>
         </main>
     </section>
 </template>
@@ -21,11 +39,13 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 
+import BearStats from '~/components/BearStats.vue'
 import PageHeader from '~/components/PageHeader.vue'
 
 export default {
     components: {
         PageHeader,
+        BearStats,
     },
 
     data: () => {
